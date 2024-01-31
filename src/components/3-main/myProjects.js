d6108911a4cf1a093c0a5e31aa210d20edf04932
mahmouddebo2 <mahmoudderbala27@gmail.com>
2024-01-31 14:12:58 +0200

    socend commit

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -1,5 +1,43 @@
 export const myProjects = [
   {
+    title: "Nextjs & Tailwind &  Project",
+    category: ["react", "nextjs"],
+    desc: `EgyptShop is an innovative Next.js eCommerce project that brings together cutting-edge technology and seamless user experience. Developed entirely using Next.js, this website offers a dynamic and efficient platform for online shopping. The sleek and modern design is achieved through the use of Tailwind CSS, providing an attractive and intuitive interface for users.For backend functionality and content management, EgyptShop utilizes Sanity UI, a user-friendly and easily customizable solution. This choice not only facilitates smooth handling of data but also allows for easy modifications, making it a versatile and agile eCommerce platform.One of the standout features of EgyptShop is its responsive design, ensuring optimal performance on various devices and screen sizes. Whether accessed on smartphones, tablets, or desktops, users can enjoy a consistent and enjoyable browsing experience.With its powerful combination of Next.js, Tailwind CSS, and Sanity UI, EgyptShop stands as a testament to modern eCommerce development, offering a user-friendly, visually appealing, and fully responsive online shopping destination.`,
+    imgPath: ["./images/ecom.PNG"],
+    path: "https://egypt-shop-r8rs.vercel.app/",
+    git: "https://github.com/mahmouddebo2/EgyptShop",
+    more: "https://blog-smoky-xi.vercel.app/blog/egypt-shop-full-stack-next-js-e-commerce",
+  },
+
+  {
+    title: " NextJS & Tailwindcss & Mongodb Project",
+    category: ["next", "react"],
+    desc: "This website was developed using Next.js as the primary language for the frontend. Next.js is a React framework designed to facilitate the efficient and rapid development of modern web applications.MongoDB was utilized as the database for managing and storing data. MongoDB is a NoSQL database that offers high efficiency in data storage and querying.The user interface was styled and designed using Tailwind CSS, a CSS framework that enables the easy application of advanced design styles. The elements were arranged and formatted in an attractive and organized manner, providing a pleasant and engaging browsing experience.The website is fully responsive, meaning it adapts seamlessly to various screen sizes and devices. Whether you're using a smartphone, tablet, or personal computer, you'll have a smooth and consistent website experience. By employing these modern technologies and tools, the site achieves advanced functionality, an engaging user experience, and full responsiveness.",
+    imgPath: ["./images/2.PNG"],
+    path: "https://dashboard-delta-inky.vercel.app/dashboard",
+    git: "https://github.com/mahmouddebo2/dashboard",
+    more: "https://blog-smoky-xi.vercel.app/blog/dashboard-full-stack-next-js-14",
+  },
+  {
+    title: " NextJS & Tailwindcss Project",
+    category: ["next", "react", "javascript"],
+    desc: "The project was implemented using popular programming languages such as HTML and CSS, with the integration of Bootstrap to enhance the user experience and provide an attractive and user-friendly design. jQuery was also utilized to improve interaction and add dynamic features. The result is an elegant and fully responsive website, featuring a beautiful design and effective use of modern technologies",
+    imgPath: ["./images/ain.PNG"],
+    path: "https://airbnb-neon-one.vercel.app/",
+    git: "https://github.com/mahmouddebo2/Airbnb",
+    more: "https://blog-smoky-xi.vercel.app/blog/airbnb-app",
+  },
+  {
+    title: " NextJS & Tailwindcss Project",
+    category: ["next", "react", "tailwind"],
+    desc: `This project is a tourism website built using Next.js and the Tailwind CSS framework. The site boasts an advanced UI/UX design that focuses specifically on user experience and attractiveness. The user interface was exclusively developed without a backend, emphasizing the primary focus on design and user experience.Tailwind CSS was prominently utilized in the development of this project, showcasing strong skills in this language to achieve an elegant and sophisticated design. The website allows visitors to explore tourism content in a simple and engaging manner.
+    What also sets the site apart is its full responsiveness, meaning it adapts perfectly to various screen sizes and device types. Thanks to this flexible design, users can enjoy a seamless and distinctive user experience on both mobile devices and personal computers alike.`,
+    imgPath: ["./images/s.PNG"],
+    path: "https://travel-app-nine-xi.vercel.app/",
+    git: "https://github.com/mahmouddebo2/Travel-App",
+    more: "https://blog-smoky-xi.vercel.app/blog/travel-app-ui-ux-nextjs",
+  },
+  {
     title: "HTML & Bootstrap & Jquery  Project",
     category: ["css", "bootstrap"],
     desc: `This project is a beautiful and distinctive website created using HTML, Bootstrap, and enhanced with the creative touch of jQuery. The site stands out with an exceptional and aesthetic design, showcasing uniqueness in the libraries used, such as jQuery, and the utilization of Bootstrap CSS.HTML served as the primary language for building the fundamental structure of the pages, while Bootstrap enhanced the design with its user-friendly and ready-to-use components. Creative touches were added through jQuery, enhancing interactivity and dynamism in the user experience.Furthermore, the site is fully responsive, meaning it adapts perfectly to various screen sizes and device types. Thanks to these features, the website provides a seamless and engaging browsing experience across different devices.`,
@@ -18,52 +56,5 @@ export const myProjects = [
     git: "https://github.com/mahmouddebo2/Yummy-App",
     more: "https://blog-smoky-xi.vercel.app/blog/yummy-app",
   },
-  {
-    title: "Nextjs & Tailwind &  Project",
-    category: ["react", "nextjs"],
-    desc: `EgyptShop is an innovative Next.js eCommerce project that brings together cutting-edge technology and seamless user experience. Developed entirely using Next.js, this website offers a dynamic and efficient platform for online shopping. The sleek and modern design is achieved through the use of Tailwind CSS, providing an attractive and intuitive interface for users.For backend functionality and content management, EgyptShop utilizes Sanity UI, a user-friendly and easily customizable solution. This choice not only facilitates smooth handling of data but also allows for easy modifications, making it a versatile and agile eCommerce platform.One of the standout features of EgyptShop is its responsive design, ensuring optimal performance on various devices and screen sizes. Whether accessed on smartphones, tablets, or desktops, users can enjoy a consistent and enjoyable browsing experience.With its powerful combination of Next.js, Tailwind CSS, and Sanity UI, EgyptShop stands as a testament to modern eCommerce development, offering a user-friendly, visually appealing, and fully responsive online shopping destination.`,
-    imgPath: ["./images/ecom.PNG"],
-    path: "https://egypt-shop-r8rs.vercel.app/",
-    git: "https://github.com/mahmouddebo2/EgyptShop",
-    more: "https://blog-smoky-xi.vercel.app/blog/egypt-shop-full-stack-next-js-e-commerce",
-  },
-  {
-    title: "ReactJS & Nextjs Project",
-    category: ["react", "nextjs"],
-    desc: `
-    This project was developed using React.js to create a movie website, providing users with an easy and efficient way to explore the world of cinema. The site allows users to access top movie ratings and the latest releases through the utilization of The Movie Database (TMDb) API.Modern technologies such as React.js were employed to deliver a smooth and dynamic user experience. The fetch API was implemented to retrieve movie data directly from TMDb, enabling real-time updates on movie ratings and related information.Thanks to its fully responsive design, the website allows users to access and navigate it seamlessly across various screen sizes and device types. Whether using smartphones, tablets, or personal computers, users can enjoy a flexible and responsive browsing experience. By leveraging these advanced technologies, the project serves as an excellent resource for cinema enthusiasts seeking easy and reliable access to up-to-date information about the latest movies and their ratings.`,
-    imgPath: ["./images/movies.PNG"],
-    path: "https://movies-an.vercel.app/",
-    git: "https://github.com/mahmouddebo2/movies-an",
-    more:"https://blog-smoky-xi.vercel.app/blog/movies-app-reactjs"
-  },
-  {
-    title: " NextJS & Tailwindcss & Mongodb Project",
-    category: ["next", "react"],
-    desc: "This website was developed using Next.js as the primary language for the frontend. Next.js is a React framework designed to facilitate the efficient and rapid development of modern web applications.MongoDB was utilized as the database for managing and storing data. MongoDB is a NoSQL database that offers high efficiency in data storage and querying.The user interface was styled and designed using Tailwind CSS, a CSS framework that enables the easy application of advanced design styles. The elements were arranged and formatted in an attractive and organized manner, providing a pleasant and engaging browsing experience.The website is fully responsive, meaning it adapts seamlessly to various screen sizes and devices. Whether you're using a smartphone, tablet, or personal computer, you'll have a smooth and consistent website experience. By employing these modern technologies and tools, the site achieves advanced functionality, an engaging user experience, and full responsiveness.",
-    imgPath: ["./images/2.PNG"],
-    path: "https://dashboard-delta-inky.vercel.app/dashboard",
-    git: "https://github.com/mahmouddebo2/dashboard",
-    more:"https://blog-smoky-xi.vercel.app/blog/dashboard-full-stack-next-js-14"
-  },
-  {
-    title: " NextJS & Tailwindcss Project",
-    category: ["next", "react" ,"javascript"],
-    desc: "The project was implemented using popular programming languages such as HTML and CSS, with the integration of Bootstrap to enhance the user experience and provide an attractive and user-friendly design. jQuery was also utilized to improve interaction and add dynamic features. The result is an elegant and fully responsive website, featuring a beautiful design and effective use of modern technologies",
-    imgPath: ["./images/ain.PNG"],
-    path: "https://airbnb-neon-one.vercel.app/",
-    git: "https://github.com/mahmouddebo2/Airbnb",
-    more:"https://blog-smoky-xi.vercel.app/blog/airbnb-app"
-  },
 
-  {
-    title: " NextJS & Tailwindcss Project",
-    category: ["next", "react","tailwind"],
-    desc: `This project is a tourism website built using Next.js and the Tailwind CSS framework. The site boasts an advanced UI/UX design that focuses specifically on user experience and attractiveness. The user interface was exclusively developed without a backend, emphasizing the primary focus on design and user experience.Tailwind CSS was prominently utilized in the development of this project, showcasing strong skills in this language to achieve an elegant and sophisticated design. The website allows visitors to explore tourism content in a simple and engaging manner.
-    What also sets the site apart is its full responsiveness, meaning it adapts perfectly to various screen sizes and device types. Thanks to this flexible design, users can enjoy a seamless and distinctive user experience on both mobile devices and personal computers alike.`,
-    imgPath: ["./images/s.PNG"],
-    path: "https://travel-app-nine-xi.vercel.app/",
-    git: "https://github.com/mahmouddebo2/Travel-App",
-    more: "https://blog-smoky-xi.vercel.app/blog/travel-app-ui-ux-nextjs",
-  },
 ];
