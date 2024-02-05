35019ef4a403917b5e285931bc985f3838164bf8
mahmouddebo2 <mahmoudderbala27@gmail.com>
2024-02-05 17:06:11 +0200

    links edit

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -1,32 +1,41 @@
-import  './hero.css'
+/* eslint-disable react/jsx-no-target-blank */
+import "./hero.css";
 import Lottie from "lottie-react";
-import heroAnimation from "../../animation/developer.json"
+import heroAnimation from "../../animation/developer.json";
 
 export default function Hero() {
   return (
-    <section className='hero flex'>
-      <div className='left-section '>
-<div className='parent-avatar flex'>
-<img src="./me.png" alt="Mahmoud Avatar" className='avatar'/>
-<div className='icon-verified'/>
-</div>
-<h1 className='title'>Frontend Developer  </h1>
-      <p className='subtitle'>Dedicated and skilled Frontend Developer with a passion for creating responsive and user-friendly web
-applications using React JS And Next JS With more than a year of experience in the field, I have a proven track
-Record of delivering high-quality software solutions for clients across various industries. My expertise extends to
-Crafting visually engaging and interactive interfaces that enhance the overall user experience.
-</p>
-<div className="social-icons flex">
-<div className=' icon icon-twitter'></div>
-<div className='icon icon-instagram'></div>
-<div className='icon icon-github'></div>
-<div className='icon icon-linkedin'></div>
-</div>
+    <section className="hero flex">
+      <div className="left-section ">
+        <div className="parent-avatar flex">
+          <img src="./me.png" alt="Mahmoud Avatar" className="avatar" />
+          <div className="icon-verified" />
+        </div>
+        <h1 className="title">Frontend Developer </h1>
+        <p className="subtitle">
+          Dedicated and skilled Frontend Developer with a passion for creating
+          responsive and user-friendly web applications using React JS And Next
+          JS With more than a year of experience in the field, I have a proven
+          track Record of delivering high-quality software solutions for clients
+          across various industries. My expertise extends to Crafting visually
+          engaging and interactive interfaces that enhance the overall user
+          experience.
+        </p>
+        <div className="social-icons flex">
+          <a href="https://www.instagram.com/mahmoudderbala4/" target="_blank">
+            <div className="icon icon-instagram"></div>
+          </a>
+          <a href="https://github.com/mahmouddebo2/" target="_blank">
+            <div className="icon icon-github"></div>
+          </a>
+          <a href="https://www.linkedin.com/in/derbaladeveloper/" target="_blank">
+            <div className="icon icon-linkedin"></div>
+          </a>
+        </div>
       </div>
-      <div className='right-section animation  '>
-      <Lottie className='heroanimation'   animationData={heroAnimation} />
-
+      <div className="right-section animation  ">
+        <Lottie className="heroanimation" animationData={heroAnimation} />
       </div>
     </section>
-  )
+  );
 }
